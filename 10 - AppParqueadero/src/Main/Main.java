939ad8d42625cc646e079bf0b38e16ce88c28da4
Ramirez-05 <ramirezcardona2204@gmail.com>
2023-07-28 10:31:00 -0500

    20/07/23

--- 10 - AppParqueadero/src/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-public class Main {
-    public static void main(String[] args) {
-        
-        LoginV2 ventana = new LoginV2();
-        
-    }
-}
---
+++ 10 -AppPaqueadero/src/Clases/DataBase.java
@@ -0,0 +1,85 @@
+package Clases;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DataBase {
+    
+    Connection conexion;
+    Statement manipularDB;
+    
+    public DataBase (){
+           
+        String hostname = "localhost";
+	String puerto = "3306";
+	String databasename = "app_java";
+	String user = "root";
+	String password = "";
+        
+        String url = "jdbc:mysql://"+hostname+":"+puerto+"/"+databasename;
+        
+  
+        
+        try {
+            this.conexion = DriverManager.getConnection(url, user, password);
+            this.manipularDB = this.conexion.createStatement();
+            System.out.println("Conexion a DataBase Exitosa");
+        } catch (SQLException ex) {
+            System.out.println("Error en conexion a base de datos"+ex.getMessage());
+        }     
+        
+    }
+    
+    
+     public boolean insertarPersona(String cedula, String nombres, String apellidos, String telefono, String direccion, String email){
+            boolean respuesta = false;
+            
+        try {
+            String consulta = "INSERT INTO personas (cedula, nombres, apellidos, telefono, direccion, email) VALUES ('"+cedula+"','"+nombres+"','"+apellidos+"','"+telefono+"','"+direccion+"','"+email+"')";
+            int resultado = this.manipularDB.executeUpdate(consulta);
+            
+            if(resultado == 1){
+                respuesta = true;
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar: "+ex.getMessage());   
+         
+           
+        }
+     return respuesta;
+     
+     }
+     
+     public ResultSet listaPersonas(){
+         
+         ResultSet listado = null;
+         
+        
+        try {
+            String consulta = "SELECT * FROM personas";
+            listado = this.manipularDB.executeQuery(consulta);
+            
+            listado.next();
+            
+            if(listado.getRow()==1){
+                return listado;
+            }else{
+                return null;
+            }
+            
+            
+        } catch (SQLException ex) {
+            System.out.println("Error en SELECT"+ex.getMessage());
+        }
+        
+        
+        return listado;
+     }
+     
+     
+     
+     
+}
