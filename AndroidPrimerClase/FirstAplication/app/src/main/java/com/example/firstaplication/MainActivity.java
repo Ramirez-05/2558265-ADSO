5b3c6e678175fbb616ab053bd5fc12d6b1c00b6d
Ramirez-05 <ramirezcardona2204@gmail.com>
2023-11-10 11:14:41 -0500

    Hola

---
+++ AndroidPrimerClase/FirstAplication/app/src/main/java/com/example/firstaplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.firstaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText campo_id;
+    EditText campo_nombre;
+    EditText campo_email;
+    Button btn_registrer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campo_id = findViewById(R.id.campo_id);
+        campo_nombre = findViewById(R.id.campo_nombre);
+        campo_email = findViewById(R.id.campo_email);
+        btn_registrer = findViewById(R.id.btn_registrer);
+    }
+
+    public void registrarPersona(View vista){
+        String id = campo_id.getText().toString();
+        String nombre = campo_nombre.getText().toString();
+        String email = campo_email.getText().toString();
+        System.out.println("id: "+id);
+        System.out.println("nombre: "+nombre);
+        System.out.println("email: "+email);
+    }
+
+}
